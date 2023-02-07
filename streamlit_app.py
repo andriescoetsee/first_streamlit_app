3994efe161006206bae6c33ce2a1b8b68c084268
Andries Coetsee <34986276+andriescoetsee@users.noreply.github.com>
2023-02-07 10:04:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ streamlit.title('My Parents New Healthy Diner is here, can you believe it!')
 
 streamlit.header('Breakfast Menu')
 streamlit.markdown('* Omega 3 & Blueberry Oatmeal')
-streamlit..markdown('* Kale, Spinach & Rocket Smoothie')
-streamlit..markdown('* Hard-Boiled Free-Range Egg')
-streamlit..markdown('* Avocado Toast')
+streamlit.markdown('* Kale, Spinach & Rocket Smoothie')
+streamlit.markdown('* Hard-Boiled Free-Range Egg')
+streamlit.markdown('* Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
